680a35f7a8182dc416a22e95e642d46be7447075
Jamie Thomas <jamiemthomas@hotmail.com>
2018-10-31 08:36:06 -0400

    Shortening component names

--- src/CognitoForm.vue
+++ src/CognitoForm.vue
@@ -6,78 +6,78 @@
 		</header>
 
 		<div class="c-body c-wrapper">
-			<cognito-page-progress />
-			<cognito-page page=1>
-				<cognito-section type="field" required="true" title="Rating Scale" column="1" colspan="24">
-					<cognito-rating-scale></cognito-rating-scale>
-				</cognito-section>
-				<cognito-section type="field" required="true" title="Rating Scale" column="1" colspan="15">
-					<cognito-rating-scale></cognito-rating-scale>
-				</cognito-section>
-				<cognito-section type="field" required="true" title="Rating Scale" column="16" colspan="9">
-					<cognito-rating-scale></cognito-rating-scale>
-				</cognito-section>
-				<cognito-section type="repeating-section" title="Section Title" help="This is a section help" column="1" colspan="12">
-					<cognito-section type="field"  help="check it if it is true." column="1" colspan="12">
-						<cognito-checkbox label="Hello" />
-					</cognito-section>
-					<cognito-section type="field" title="Your Email" column="13" colspan="12">
-						<cognito-text type="email" />
-					</cognito-section>
-				</cognito-section>
-				<cognito-section type="section" title="Section Title" help="This is a section help" column="13" colspan="12">
-					<cognito-section type="field" title="Your Email" column="7" colspan="6">
-						<cognito-text type="email" />
-					</cognito-section>
-					<cognito-section type="field" required="true" title="Your Email" column="13" colspan="18">
-						<cognito-text type="email" />
-					</cognito-section>
-				</cognito-section>
-				<cognito-section type="field" title="Do you agree?" column="1" colspan="6">
-					<cognito-checkbox />
-				</cognito-section>
-				<cognito-section type="field" title="Your Email" error="Something is wrong" column="1" colspan="6">
-					<cognito-text type="email" />
-				</cognito-section>
-				<cognito-section type="field" title="Your Name" column="1" colspan="12">
-					<cognito-text type="text" />
-				</cognito-section>
-				<cognito-section type="field" title="Your Name" column="13" colspan="12">
-					<cognito-text type="text" />
-				</cognito-section>
-			</cognito-page>
-			<cognito-page page=2>
-				<cognito-section type="field" title="Address" help="The place you live." error="That's not your address!" column="1" colspan="24">
-					<cognito-address />
-				</cognito-section>
-				<cognito-button-section />
-			</cognito-page>
+			<c-page-progress />
+			<c-page page=1>
+				<c-section type="field" required="true" title="Rating Scale" column="1" colspan="24">
+					<c-rating-scale />
+				</c-section>
+				<c-section type="field" required="true" title="Rating Scale" column="1" colspan="15">
+					<c-rating-scale />
+				</c-section>
+				<c-section type="field" required="true" title="Rating Scale" column="16" colspan="9">
+					<c-rating-scale />
+				</c-section>
+				<c-section type="repeating-section" title="Section Title" help="This is a section help" column="1" colspan="12">
+					<c-section type="field"  help="check it if it is true." column="1" colspan="12">
+						<c-checkbox label="Hello" />
+					</c-section>
+					<c-section type="field" title="Your Email" column="13" colspan="12">
+						<c-text type="email" />
+					</c-section>
+				</c-section>
+				<c-section type="section" title="Section Title" help="This is a section help" column="13" colspan="12">
+					<c-section type="field" title="Your Email" column="7" colspan="6">
+						<c-text type="email" />
+					</c-section>
+					<c-section type="field" required="true" title="Your Email" column="13" colspan="18">
+						<c-text type="email" />
+					</c-section>
+				</c-section>
+				<c-section type="field" title="Do you agree?" column="1" colspan="6">
+					<c-checkbox />
+				</c-section>
+				<c-section type="field" title="Your Email" error="Something is wrong" column="1" colspan="6">
+					<c-text type="email" />
+				</c-section>
+				<c-section type="field" title="Your Name" column="1" colspan="12">
+					<c-text type="text" />
+				</c-section>
+				<c-section type="field" title="Your Name" column="13" colspan="12">
+					<c-text type="text" />
+				</c-section>
+			</c-page>
+			<c-page page=2>
+				<c-section type="field" title="Address" help="The place you live." error="That's not your address!" column="1" colspan="24">
+					<c-address />
+				</c-section>
+				<c-button-section />
+			</c-page>
 		</div>
   </form>
 </template>
 
 <script>
-	import CognitoPageProgress from './components/CognitoPageProgress.vue';
-	import CognitoPage from './components/CognitoPage.vue';
-	import CognitoSection from './components/CognitoSection.vue';
-	import CognitoCheckbox from './components/CognitoCheckbox.vue';
-	import CognitoText from './components/CognitoText.vue';
-	import CognitoAddress from './components/CognitoAddress.vue';
-	import CognitoRatingScale from './components/CognitoRatingScale.vue';
+	import CPageProgress from './components/CognitoPageProgress.vue';
+	import CPage from './components/CognitoPage.vue';
+	import CSection from './components/CognitoSection.vue';
+	import CCheckbox from './components/CognitoCheckbox.vue';
+	import CText from './components/CognitoText.vue';
+	import CAddress from './components/CognitoAddress.vue';
+	import CRatingScale from './components/CognitoRatingScale.vue';
 
-	import CognitoButtonSection from './components/CognitoButtonSection.vue';
+	import CButtonSection from './components/CognitoButtonSection.vue';
 
 	export default {
 		name: 'c-form',
 		components: {
-			CognitoPageProgress,
-			CognitoPage,
-			CognitoSection,
-			CognitoCheckbox,
-			CognitoText,
-			CognitoAddress,
-			CognitoButtonSection,
-			CognitoRatingScale
+			CPageProgress,
+			CPage,
+			CSection,
+			CCheckbox,
+			CText,
+			CAddress,
+			CButtonSection,
+			CRatingScale
 		}
 	}
 </script>
